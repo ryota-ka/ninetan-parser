a93c8cd0eb32d69920f950c70e43fd25cb09acf3
Ryota Kameoka <kameoka.ryota@gmail.com>
2016-09-04 19:35:07 +0900

    Add ninetan formatter as main executable

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,14 @@
 module Main where
 
-import Lib
+import Web.Ninetan (Forecast(..), getCurrentYearInJST, Location(..), Ninetan(..), parseNinetan)
+import Data.List (intercalate)
+import Data.Time.LocalTime (localDay, localTimeOfDay)
 
 main :: IO ()
-main = someFunc
+main = do
+    Ninetan (Location prefecture place) forecasts <- either (const fail) id <$> (parseNinetan <$> getCurrentYearInJST <*> getContents)
+    putStrLn prefecture
+    putStrLn place
+    mapM_ (putStrLn . formatForecast) forecasts
+    where fail = error "Parse error"
+          formatForecast (Forecast t a b c d e f g) = intercalate " " $ show (localDay t) : show (localTimeOfDay t) : map show [a, b, c, d, e, f, g]
